d1c58923a97db5ccc9bf10508a243ce84fdca50b
Valentin Fevre <valentaf1803@gmail.com>
2025-02-25 12:13:58 +0100

    proper without comment

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,8 @@
-import io
 import os
 
 from app import app
 import pandas as pd
-# import sqlalchemy as sa
 from flask import render_template, flash, redirect, url_for, request
-# from flask_login import current_user, login_user
-# from flask_login import logout_user, login_required
-# from sqlalchemy import desc
 from werkzeug.utils import secure_filename
 # import cv2
 from app.utils import allowed_file
@@ -27,7 +22,7 @@ def upload_file():
             file.save(filepath)
             
             # Appel du modèle pour la détection
-            output_path = os.path.join("static", filename)
+            # output_path = os.path.join("static", filename)
             # annotated_image = your_model_script.detect_varroa(filepath)  # Fonction à implémenter dans ton fichier modèle
             # cv2.imwrite(output_path, annotated_image)
                 
